7fe5fbafd3962821df839140f5d5bfe89ccca818
césar <chyss.mail@gmail.com>
2026-05-09 12:35:49 +0200

    add SQL scripts for creating and rolling back database and users table

---
+++ .deploy/sql/v1.0/02-create-users-table-rollback.sql
@@ -0,0 +1,3 @@
+USE task_manager;
+
+DROP TABLE IF EXISTS users;
---
+++ .deploy/sql/v1.0/02-create-users-table.sql
@@ -0,0 +1,9 @@
+USE task_manager;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(150) UNIQUE,
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
