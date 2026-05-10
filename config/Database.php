154e0602f261b08d5e6187353d6e5299c8fcde76
césar <chyss.mail@gmail.com>
2026-05-10 12:34:31 +0200

    feat: create Database connection class for MySQL

---
+++ config/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Config;
+
+use PDO;
+
+class Database
+{
+    public static function connect(): PDO
+    {
+        return new PDO(
+            "mysql:host=127.0.0.1;port=3306;dbname=task_manager;charset=utf8mb4",
+            "taskManagerUser",
+            "secret",
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ]
+        );
+    }
+}
