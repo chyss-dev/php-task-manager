823600beb2d3cddd0711ca23a1bf9740ddcab148
césar <chyss.mail@gmail.com>
2026-05-13 11:18:06 +0200

    feat: refactor Database connection to use environment variables

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -9,9 +9,13 @@ class Database
     public static function connect(): PDO
     {
         return new PDO(
-            "mysql:host=127.0.0.1;port=3306;dbname=task_manager;charset=utf8mb4",
-            "taskManagerUser",
-            "secret",
+            sprintf(
+                "mysql:host=%s;port=3306;dbname=%s;charset=utf8mb4",
+                $_ENV["DB_HOST"],
+                $_ENV["DB_NAME"]
+            ),
+            $_ENV["DB_USER"],
+            $_ENV["DB_PASS"],
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             ]
