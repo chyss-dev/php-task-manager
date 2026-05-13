7ba1e6cfd2f56d19baf9507f8ee89713ab994bfc
césar <chyss.mail@gmail.com>
2026-05-13 09:46:38 +0200

    feat: integrate ProfileController and update AuthService with TokenRepository

--- public/index.php
+++ public/index.php
@@ -4,24 +4,31 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Core\Router;
 use App\Repository\UserRepository;
+use App\Repository\TokenRepository;
 use App\Service\UserService;
 use App\Service\AuthService;
 use App\Controller\UserController;
 use App\Controller\AuthController;
+use App\Controller\ProfileController;
 
 $router = new Router();
 
 $userRepository = new UserRepository();
+$tokenRepository = new TokenRepository();
 
 $userService = new UserService($userRepository);
-$authService = new AuthService($userRepository);
+$authService = new AuthService($userRepository, $tokenRepository);
 
 $userController = new UserController($userService);
 $authController = new AuthController($authService);
+$profileController = new ProfileController();
 
 $router->get('/users', [$userController, 'index']);
+$router->get('/profile', [$profileController, 'index']);
+
 $router->post('/users', [$userController, 'store']);
 $router->post('/login', [$authController, 'login']);
+
 
 $router->dispatch(
     $_SERVER['REQUEST_METHOD'],
